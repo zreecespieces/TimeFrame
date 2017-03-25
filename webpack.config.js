db03b29061813c1daeb81db5ae731c9090da8e35
Zachary Reece <zdreece@mail.fhsu.edu>
2017-03-25 15:23:13 -0500

    React and Webpack Config Complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
 module.exports = {
   context: path.join(__dirname, "src"),
@@ -20,12 +26,13 @@ module.exports = {
     ]
   },
   output: {
-    path: __dirname + "/src/",
+    path: __dirname + "/src/build",
     filename: "client.min.js"
   },
-  plugins: debug ? [] : [
+  plugins: debug ? [HTMLWebpackPluginConfig] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    HTMLWebpackPluginConfig,
   ],
 };
