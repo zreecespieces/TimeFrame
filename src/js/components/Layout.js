4dd6c32459182c2ed96a55bae6228b458a23ed00
Zachary Reece <zdreece@mail.fhsu.edu>
2017-03-27 03:47:02 -0500

    Added font and a Title component

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,9 +1,9 @@
 var React = require("react");
-var ReactDOM = require("react-dom");
+var Title = require("./Title");
 
 var Layout = React.createClass({
   render: function() {
-    return <h1>Hello world!</h1>;
+    return <Title/>;
   }
 });
 
---
+++ src/js/components/Title.js
@@ -0,0 +1,9 @@
+var React = require("react");
+
+var Title = React.createClass({
+  render: function() {
+    return <div id="Title">Timeframe</div>
+  }
+});
+
+module.exports = Title;
