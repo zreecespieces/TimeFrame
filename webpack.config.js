7b3c856eb7500e5b54c75fe3b3fa549510d66a85
zreecespieces <zdreece@mail.fhsu.edu>
2017-04-28 18:35:06 -0500

    Removed unnecessary scripts in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,8 @@
-var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
-var HTMLWebpackPlugin = require('html-webpack-plugin');
-var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: __dirname + '/src/static/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
 
 module.exports = {
   context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : "cheap-module-source-map",
   entry: "./client.jsx",
   module: {
     loaders: [
@@ -28,9 +20,5 @@ module.exports = {
   output: {
     path: __dirname + "/src/static",
     filename: "client.min.js"
-  },
-  plugins: debug ? [HTMLWebpackPluginConfig] : [
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    HTMLWebpackPluginConfig,
-  ],
+  }
 };
