2d50a5ac9144f6f54cd2cfe46374138a188f163f
Zachary Reece <zachdreece@gmail.com>
2018-01-27 15:18:03 -0600

    Beginning refactor of project to use ES6, Redux, and other more modern practices

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
-var webpack = require('webpack');
-var path = require('path');
-
 module.exports = {
-  context: path.join(__dirname, "src"),
-  entry: "./client.jsx",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-        }
-      }
-    ]
-  },
+  entry: [
+    './src/index.js'
+  ],
   output: {
-    path: __dirname + "/src/static",
-    filename: "client.min.js"
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
   }
 };
