1a6014bf8669827fad9126fbc84a2a56382bb0de
Zachary Reece <zachdreece@gmail.com>
2018-01-28 19:05:05 -0600

    Added title image and background color

--- src/components/app.js
+++ src/components/app.js
@@ -6,8 +6,9 @@ import Result from './result';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <div>React simple starter</div>
+      <div id="wrapper">
+        <img src="/src/img/title.png" className="img-fluid" alt="Responsive image"/>
+        React simple starter
         <Sentence />
         <Form />
         <Result />
