87f3f6c2dee5842bbddc4cb728b3089aad15e07f
Zachary Reece <zachdreece@gmail.com>
2018-01-30 17:49:54 -0600

    Added fetchScript action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,29 @@
 import axios from 'axios';
 
-const ROOT_URL = 'http://www.omdbapi.com/?apikey=eb9fc4d2&';
+const RUNTIME_URL = 'http://www.omdbapi.com/?apikey=eb9fc4d2&';
+const SCRIPT_URL = 'http://www.imsdb.com/scripts/';
 
 export const FETCH_RUNTIME = 'FETCH_RUNTIME';
+export const FETCH_SCRIPT = 'FETCH_SCRIPT';
 
 export function fetchRuntime(movie, callback) {
-  const url = `${ROOT_URL}t=${movie}`;
+  const url = `${RUNTIME_URL}t=${movie}`;
   const request = axios.get(url)
   .then(() => callback());
 
   return {
     type: FETCH_RUNTIME,
     payload: request
+  }
+}
+
+export function fetchScript(movie, callback) {
+  const url = `${SCRIPT_URL}${movie}.html`;
+  const request = axios.get(url)
+  .then(() => callback());
+
+  return {
+    type: FETCH_SCRIPT,
+    payload: request;
   }
 }
