254db084770ed0bf025ae74260421549a1843cc9
Zachary Reece <zachdreece@gmail.com>
2018-01-30 17:28:39 -0600

    More setup on fetchRuntime action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,12 @@ import axios from 'axios';
 
 const ROOT_URL = 'http://www.omdbapi.com/?apikey=eb9fc4d2&';
 
-export function fetchRuntime(movie) {
+export const FETCH_RUNTIME = 'FETCH_RUNTIME';
+
+export function fetchRuntime(movie, callback) {
   const url = `${ROOT_URL}t=${movie}`;
-  const request = axios.get(url);
+  const request = axios.get(url)
+  .then(() => callback());
 
   return {
     type: FETCH_RUNTIME,
