d9140a92df434310a9348c39db574f7fee9453a0
Zachary Reece <zachdreece@gmail.com>
2018-01-30 16:00:17 -0600

    Set up fetchRuntime action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,13 @@
 import axios from 'axios';
 
 const ROOT_URL = 'http://www.omdbapi.com/?apikey=eb9fc4d2&';
+
+export function fetchRuntime(movie) {
+  const url = `${ROOT_URL}t=${movie}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_RUNTIME,
+    payload: request
+  }
+}
--- src/components/sentence.js
+++ src/components/sentence.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Sentence extends Component {
   render(){
-    return <div className="col-xs-4" id="sentence">Watching blah but every time they say blah it's repalced with blah would take: </div>;
+    return <div className="col-xs-4" id="sentence">Watching blah but every time they say blah it's replaced with blah would take: </div>;
   }
 }
 
