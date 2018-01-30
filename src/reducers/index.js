dcfc8d9b894c72dd417488f766df6f27653b23dd
Zachary Reece <zachdreece@gmail.com>
2018-01-30 17:56:16 -0600

    Wiring together reducers with combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import MoviesReducer from './reducer_movies';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  movies: MoviesReducer,
+  form: formReducer
 });
 
 export default rootReducer;
