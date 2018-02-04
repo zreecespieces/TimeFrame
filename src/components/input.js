886d36b94417193445455932e7b5df70d67e7f18
Zachary Reece <zachdreece@gmail.com>
2018-02-04 13:41:10 -0600

    Added increment and decrement button to test out the adding and removing layers functions

--- src/components/input.js
+++ src/components/input.js
@@ -4,11 +4,32 @@ import { bindActionCreators } from 'redux';
 import { fetchRuntime, fetchScript } from '../actions';
 
 class Input extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { layerNumber: 1 };
+
+    this.addLayer = this.addLayer.bind(this);
+    this.removeLayer = this.removeLayer.bind(this);
+  }
+
+  addLayer() {
+    var newLayer = this.state.layerNumber + 1;
+    this.setState({layerNumber: newLayer });
+  }
+
+  removeLayer() {
+    var newLayer = this.state.layerNumber - 1;
+    this.setState({layerNumber: newLayer });
+  }
+
   render(){
     return (
       <div className="col-xs-4">
         <button className="btn btn-primary" onClick={this.props.fetchRuntime}>Runtime</button>
         <button className="btn btn-primary" onClick={this.props.fetchScript}>Script</button>
+        <button className="btn btn-primary" onClick={this.addLayer}>Increment</button>
+        <button className="btn btn-primary" onClick={this.removeLayer}>Decrement</button>
       </div>
     );
   }
