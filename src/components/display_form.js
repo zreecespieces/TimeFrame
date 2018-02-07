e642493f25f82437cb9211dede3f3d5204c69523
Zachary Reece <zachdreece@gmail.com>
2018-02-07 00:50:05 -0600

    Submit button will now run script and runtime functions for all movies typed into any number of fields

--- src/components/display_form.js
+++ src/components/display_form.js
@@ -27,9 +27,21 @@ class DisplayForm extends Component {
   }
 
   onSubmit(values) {
-    console.log(values);
-    this.props.fetchRuntime(values.movie0);
-    this.props.fetchScript(values.movie0);
+    var movies = [], i = 0, len = this.state.layerNumber;
+    while (++i <= len) movies.push(`movie${i-1}`);
+
+    var keywords = [], i = 0, len = this.state.layerNumber;
+    while (++i <= len) keywords.push(`keyword${i-1}`);
+
+    movies.map((e) =>
+      this.props.fetchRuntime(eval(`values.${e}`))
+    );
+
+    movies.map((e) =>
+      this.props.fetchScript(eval(`values.${e}`))
+    );
+  //  this.props.fetchRuntime(values.movie0);
+  //  this.props.fetchScript(values.movie0);
   }
 
   render(){
