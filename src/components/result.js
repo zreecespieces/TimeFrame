ae09fef0da15d75b792f936fc1996034d68547ca
Zachary Reece <zachdreece@gmail.com>
2018-02-10 13:37:55 -0600

    Calculation for how long it would take to watch the first entered movie, but every time they said the specified keyword it was replaced with the next entered movie, is now rendered to the screen

--- src/components/result.js
+++ src/components/result.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import humanizeDuration from 'humanize-duration';
 
 class Result extends Component {
+  constructor(props) {
+    super(props);
+
+    this.findResult = this.findResult.bind(this);
+  }
+
+  findResult(runtimes, keywords) {
+    const layerRuntimes = runtimes;
+    const layerKeywordCount = keywords;
+
+    var totalRuntime = (layerRuntimes[0] * layerKeywordCount[0]) + layerRuntimes[1];
+
+    for (var count = 1; count < runtimes.length - 1; count++) {
+        totalRuntime = (totalRuntime * layerKeywordCount[count]) + layerRuntimes[count + 1];
+      }
+    return humanizeDuration(totalRuntime * 60000);
+  }
+
   render(){
     return (
-      <div className="col-xs-4">
-        This is the runtimes array: {this.props.runtimes}
-        <br />
-        This is the keywords array: {this.props.keywords}
+      <div id="result" className="col-xs-4">
+        {this.findResult(this.props.runtimes, this.props.keywords)}
       </div>
     );
   }
