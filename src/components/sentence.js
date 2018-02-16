6158e7c54def8e2e0892b60314c114fef44b7d05
Zachary Reece <zachdreece@gmail.com>
2018-02-15 19:29:06 -0600

    The first movie is now hard coded to render on screen as it is being typed in

--- src/components/sentence.js
+++ src/components/sentence.js
@@ -4,7 +4,13 @@ import { connect } from 'react-redux';
 class Sentence extends Component {
   render(){
     return (
-      <div className="col-xs-4" id="sentence">Watching blah but every time they say blah it's replaced with blah would take: </div>
+      <div className="col-xs-4" id="sentence">Watching
+        {!this.props.movie0 ?
+            <div>Loading...</div>
+        :
+            <div>{this.props.movie0.values.movie0}</div>} 
+        blah but every time they say blah it's replaced with blah would take:
+      </div>
     );
   }
 }
