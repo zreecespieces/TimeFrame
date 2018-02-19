df7cbd1e2b9666a6d7c31a00485735872b95cf63
Zachary Reece <zachdreece@gmail.com>
2018-02-18 22:19:26 -0600

    Keywords and movies are now being separted in a temporary way inside Sentence component

--- src/components/sentence.js
+++ src/components/sentence.js
@@ -3,14 +3,21 @@ import { connect } from 'react-redux';
 
 class Sentence extends Component {
   render(){
+    const allMovies = [];
+    const allKeywords = [];
+
     return (
       <div className="col-xs-4" id="sentence">Watching
         {!this.props.formValues ?
             ' '
         :
-            <div>{Object.values(this.props.formValues.values).map((e) => {
-              return e;
-            })}</div>}
+            <div>{Object.keys(this.props.formValues.values).map((e) => {
+              if (e.includes("movie")) {
+                allMovies.push(e);
+              } else {
+                allKeywords.push(e);
+              }
+            })}{`The movies are: ${allMovies}, and the keywords are: ${allKeywords}`}</div>}
         blah but every time they say blah it's replaced with blah would take:
       </div>
     );
